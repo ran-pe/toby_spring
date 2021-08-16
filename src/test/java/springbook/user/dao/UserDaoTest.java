628c1f45ca16f83eec63121123a84285f681ee06
youngran.kwon <youngran.kwon@lemonhc.com>
2021-08-16 12:41:15 +0900

    토비의스프링 2장 테스트_완료

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,25 +1,49 @@
 package springbook.user.dao;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "/test-applicationContext.xml")
+//@DirtiesContext
 public class UserDaoTest {
+
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @Before
+    public void setUp() {
+        this.user1 = new User("horany1", "영란1", "eodfks09");
+        this.user2 = new User("horany2", "영란2", "eodfks09");
+        this.user3 = new User("horany3", "영란3", "eodfks09");
+
+        userDao = new UserDao();
+        DataSource dataSource = new SingleConnectionDataSource(
+                "jdbc:mysql://localhost/springbookTest?serverTimezone=UTC", "youngran", "eodfks09", true
+        );
+        userDao.setDataSource(dataSource);
+    }
 
     @Test
     public void addAndGet() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-        User user1 = new User("horany1", "영란1", "eodfks09");
-        User user2 = new User("horany2", "영란2", "eodfks09");
-
         userDao.deleteAll();
         assertThat(userDao.getCount(), is(0));
 
@@ -38,13 +62,6 @@ public class UserDaoTest {
 
     @Test
     public void count() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-
-        User user1 = new User("horany1", "영란1", "eodfks09");
-        User user2 = new User("horany2", "영란2", "eodfks09");
-        User user3 = new User("horany3", "영란3", "eodfks09");
-
         userDao.deleteAll();
         assertThat(userDao.getCount(), is(0));
 
@@ -60,9 +77,7 @@ public class UserDaoTest {
 
     @Test(expected = EmptyResultDataAccessException.class)  // 테스트 중에 발생할 것으로 기대하는 예외 클래스를 지정해준다.
     public void getUserFailure() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
-        UserDao userDao = context.getBean("userDao", UserDao.class);
         userDao.deleteAll();
         assertThat(userDao.getCount(), is(0));
 
